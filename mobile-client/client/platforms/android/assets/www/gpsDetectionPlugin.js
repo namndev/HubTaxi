2a5401e65f2a699ef3546eea2bf773c859bbae82
madman <madman.hust@gmail.com>
2016-03-30 18:03:36 +0700

    add android project

---
+++ mobile-client/client/platforms/android/assets/www/gpsDetectionPlugin.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin/gpsDetectionPlugin', function(require, exports, module) {
+    var exec = require('cordova/exec');
+
+    var gpsDetect = function() {};
+
+    gpsDetect.prototype.checkGPS = function(successCallback, failureCallback) {
+    	exec(successCallback, failureCallback, 'GpsDetectionPlugin', 'gpsDetection', []);
+    };
+
+    gpsDetect.prototype.switchToLocationSettings = function(successCallback, failureCallback) {
+        exec(successCallback, failureCallback, 'GpsDetectionPlugin', 'gpsActivation', []);
+    };
+
+    var gpsDetect = new gpsDetect();
+    module.exports = gpsDetect;
+});
